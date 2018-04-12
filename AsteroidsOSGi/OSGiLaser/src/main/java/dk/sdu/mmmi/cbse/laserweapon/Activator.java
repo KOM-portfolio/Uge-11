3d1faa9b9a92220f5e95380105e7d0404335c9db
agger1995 <christian_agger1995@hotmail.com>
2018-04-12 15:23:00 +0200

    Removed dependencies from OSGiExtender.
    Altered the  Activator to correctly load/unload the laser system.

--- AsteroidsOSGi/OSGiLaser/src/main/java/dk/sdu/mmmi/cbse/laserweapon/Activator.java
+++ AsteroidsOSGi/OSGiLaser/src/main/java/dk/sdu/mmmi/cbse/laserweapon/Activator.java
@@ -1,19 +1,24 @@
 package dk.sdu.mmmi.cbse.laserweapon;
 
+import dk.sdu.mmmi.cbse.common.bullet.BulletSPI;
 import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 
 public class Activator implements BundleActivator {
-    private ServiceRegistration registrate;
+    private ServiceRegistration entityRegistrate;
+    private ServiceRegistration laserRegistrate;
 
     public void start(BundleContext context) throws Exception {
-        this.registrate = context.registerService(IEntityProcessingService.class, new LaserSystem(), null);
+        LaserSystem laserImpl = new LaserSystem();
+        this.entityRegistrate = context.registerService(IEntityProcessingService.class, laserImpl, null);
+        this.laserRegistrate = context.registerService(BulletSPI.class, laserImpl, null);
     }
 
     public void stop(BundleContext context) throws Exception {
-        this.registrate.unregister();
+        this.entityRegistrate.unregister();
+        this.laserRegistrate.unregister();
     }
 
 }
